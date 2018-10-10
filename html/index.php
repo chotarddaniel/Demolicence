018394672b3cd0ff29d644f410e1c2ab6512e935
chotarddaniel <31211323+chotarddaniel@users.noreply.github.com>
2018-10-10 15:51:24 +0200

    BDD, 3 button
    Mise en place de la Bdd et des bouton radio
    Connexton a la base de donnée

---
+++ html/index.php
@@ -0,0 +1,25 @@
+<!Doctype <!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Page Title</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" type="text/css" media="screen" href="main.css" />
+    <script src="main.js"></script>
+</head>
+<body>
+    <?php
+        include '../php/cnx.php';
+
+        $sql = $bdd->prepare("select idCateg, nomCateg from categorie");
+        $sql  ->execute();
+        foreach($sql ->fetchAll(PDO::FETCH_ASSOC) as $ligne)
+        {
+            echo"<input type='radio' value='".$ligne['idCateg']."'>".$ligne['nomCateg'];
+        }
+
+    ?>      
+
+</body>
+</html>
