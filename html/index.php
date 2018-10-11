455c6ee8f18174cfd189701035dbbb0386a2dc15
chotarddaniel <31211323+chotarddaniel@users.noreply.github.com>
2018-10-11 02:12:46 +0200

    fail

--- html/index.php
+++ html/index.php
@@ -5,20 +5,27 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <title>Page Title</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" type="text/css" media="screen" href="main.css" />
-    <script src="main.js"></script>
+    <script src="../jQuery/jquery-3.1.1.js">
+    <script src="../js/mesfonctions.js"></script>
 </head>
+
 <body>
     <?php
         include '../php/cnx.php';
 
         $sql = $bdd->prepare("select idCateg, nomCateg from categorie");
         $sql  ->execute();
+
+       // echo "<form method='get' action='../php/getAllJeux.php'";
+
         foreach($sql ->fetchAll(PDO::FETCH_ASSOC) as $ligne)
         {
-            echo"<input type='radio' value='".$ligne['idCateg']."'>".$ligne['nomCateg'];
+           echo"<input type='radio' name='num' value='".$ligne['idCateg']."'>".$ligne['nomCateg'];
         }
-
+       // echo"<input type='submit'  value='Afficher les jeux'>";
+        echo"<input type='button'  onclick='AfficherLesJeux()' value='Afficher les jeux'>";
+        //echo"</form>"
+        echo "<div id='divJeux'></div>"
     ?>      
 
 </body>
